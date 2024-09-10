450216a3ce74690d8880a3db736967f9d862a041
Zawad Wasik Ahmed <zawadwasik@gmail.com>
2024-09-10 09:35:38 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -3,22 +3,16 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <>
+     <h1 className="text-md font-normal">
+      Hello world!
+    </h1>
+  
+    <div class="bg-blue-500 text-white p-6 shadow-lg rounded-md">
+    <h2 class="text-lg font-semibold">Container Title</h2>
+    <p>This is a container with shading and color using Tailwind CSS.</p>
     </div>
+    </>
   );
 }
 
